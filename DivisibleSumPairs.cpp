53fd0cf02ce750e5004ea221b9f2c02b4dd0f0df
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2022-10-12 16:21:29 +0900

    Update DivisibleSumPairs.cpp

--- DivisibleSumPairs.cpp
+++ DivisibleSumPairs.cpp
@@ -6,6 +6,15 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
 int divisibleSumPairs(int n, int k, vector<int> ar)
 {
     int count = 0;
